a4903d8446d13f7854d5ced92deecbfb5c148c1c
pramodkharade <kharade.pramod91@gmail.com>
2019-09-04 10:51:23 +0530

    events and method handler

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ class App extends Component {
         {name:'Shital Nalawade', age:30},
       ]
   };
+  switchHandler = () =>{
+    console.log('Switchhander is calling!!!');
+  };
   render() {
     return (
       <div className="App">
         <h1>This is react course learning</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchHandler}>Switch Name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age} > </Person>
         <Person name={this.state.person[1].name} age={this.state.person[1].age}> My Hobbies: Racing </Person>
         <Person name={this.state.person[2].name} age={this.state.person[2].age}> </Person>
