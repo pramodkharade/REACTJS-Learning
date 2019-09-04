2d8b41dc7a6c62917ac955d1b85805e6c4169c77
pramodkharade <kharade.pramod91@gmail.com>
2019-09-04 09:44:41 +0530

    outputing the dynamic content in functional components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const person =  () =>{
-    return <p>I am a person.</p>;
+    return <p>I am a person and I am { Math.floor(Math.random() * 30)} year old!.</p>;
 };
 
 export default person;
