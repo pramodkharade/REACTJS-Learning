2fd875d907fabaf41a5dc6abd33fd671e3d4404d
pramodkharade <kharade.pramod91@gmail.com>
2019-09-04 10:34:53 +0530

    use the state property in reactjs

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+      person:[
+        {name:'Pramod Kharade', age:31},
+        {name:'Kishor Vitekar', age:31},
+        {name:'Shital Nalawade', age:30},
+      ]
+  };
   render() {
     return (
       <div className="App">
         <h1>This is react course learning</h1>
-        <Person name="Pramod" age="30" > </Person>
-        <Person name="Kishor" age="30" > My Hobbies: Racing </Person>
-        <Person name="Shital" age="29" > </Person>
+        <button>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} > </Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}> My Hobbies: Racing </Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}> </Person>
       </div>
     );
   }
